359e528a744c86935ebd8f239ee48ec14c266425
DaFrenchTokio <91798175+DaFrenchTokio@users.noreply.github.com>
2022-09-04 18:41:44 +0200

    Update _1.lua

--- lib/_1.lua
+++ lib/_1.lua
@@ -1,1 +1,65 @@
-local a= {}function a:Kick(a,b)local function c(a,b)local a= Instance.new(a)for b,c in pairs(b)do a[b]=c end;return a end;local d= c('ScreenGui',{DisplayOrder=0,Enabled=true,ResetOnSpawn=true,Name='_1', Parent=game.CoreGui})local d= c('Frame',{Style=Enum.FrameStyle.Custom,Active=false,AnchorPoint=Vector2.new(0.5, 0.5),BackgroundColor3=Color3.new(0.152941, 0.152941, 0.152941),BackgroundTransparency=0,BorderColor3=Color3.new(0.105882, 0.164706, 0.207843),BorderSizePixel=1,ClipsDescendants=false,Draggable=false,Position=UDim2.new(0.5, 0, 0.5, 0),Rotation=0,Selectable=false,Size=UDim2.new(1, 0, 1.5, 0),SizeConstraint=Enum.SizeConstraint.RelativeXY,Visible=true,ZIndex=1,Name = '_1',Parent = d})local d= c('ImageLabel',{Image='rbxassetid://10819857381',ImageColor3=Color3.new(1, 1, 1),ImageRectOffset=Vector2.new(0, 0),ImageRectSize=Vector2.new(0, 0),ImageTransparency=0,ScaleType=Enum.ScaleType.Stretch,SliceCenter=Rect.new(0, 0, 0, 0),Active=false,AnchorPoint=Vector2.new(0, 0),BackgroundColor3=Color3.new(1, 1, 1),BackgroundTransparency=0,BorderColor3=Color3.new(0.105882, 0.164706, 0.207843),BorderSizePixel=1,ClipsDescendants=false,Draggable=false,Position=UDim2.new(0.443181813, 0, 0.295528471, 0),Rotation=0,Selectable=false,Size=UDim2.new(0, 150, 0, 150),SizeConstraint=Enum.SizeConstraint.RelativeXY,Visible=true,ZIndex=1,Name='_1',Parent = d})local d= c('TextLabel',{Font=Enum.Font.TitilliumWeb,FontSize=Enum.FontSize.Size60,Text='Azurite',TextColor3=Color3.new(1, 1, 1),TextScaled=false,TextSize=50,TextStrokeColor3=Color3.new(0, 0, 0),TextStrokeTransparency=1,TextTransparency=0,TextWrapped=false,TextXAlignment=Enum.TextXAlignment.Center,TextYAlignment=Enum.TextYAlignment.Center,Active=false,AnchorPoint=Vector2.new(0, 0),BackgroundColor3=Color3.new(1, 1, 1),BackgroundTransparency=1,BorderColor3=Color3.new(0.105882, 0.164706, 0.207843),BorderSizePixel=1,ClipsDescendants=false,Draggable=false,Position=UDim2.new(-0.166666672, 0, 1.13333333, 0),Rotation=0,Selectable=false,Size=UDim2.new(0, 200, 0, 50),SizeConstraint=Enum.SizeConstraint.RelativeXY,Visible=true,ZIndex=1,Name='_1',Parent = d})local d= c('Frame',{Style=Enum.FrameStyle.Custom,Active=false,AnchorPoint=Vector2.new(0, 0),BackgroundColor3=Color3.new(1, 1, 1),BackgroundTransparency=0,BorderColor3=Color3.new(0.105882, 0.164706, 0.207843),BorderSizePixel=1,ClipsDescendants=false,Draggable=false,Position=UDim2.new(-0.5, 0, -0.206666678, 0),Rotation=0,Selectable=false,Size=UDim2.new(0, 300, 0, 1),SizeConstraint=Enum.SizeConstraint.RelativeXY,Visible=true,ZIndex=1,Name = '_1',Parent = d})local d= c('Frame',{Style=Enum.FrameStyle.Custom,Active=false,AnchorPoint=Vector2.new(0, 0),BackgroundColor3=Color3.new(1, 1, 1),BackgroundTransparency=0,BorderColor3=Color3.new(0.105882, 0.164706, 0.207843),BorderSizePixel=1,ClipsDescendants=false,Draggable=false,Position=UDim2.new(-0.5, 0, 1.63333333, 0),Rotation=0,Selectable=false,Size=UDim2.new(0, 300, 0, 1),SizeConstraint=Enum.SizeConstraint.RelativeXY,Visible=true,ZIndex=1,Name = '_1',Parent = d})local c= c('TextLabel',{Font=Enum.Font.TitilliumWeb,FontSize=Enum.FontSize.Size14,Text='Hello',TextColor3=Color3.new(1, 1, 1),TextScaled=true,TextSize=14,TextStrokeColor3=Color3.new(0, 0, 0),TextStrokeTransparency=1,TextTransparency=0,TextWrapped=true,TextXAlignment=Enum.TextXAlignment.Center,TextYAlignment=Enum.TextYAlignment.Center,Active=false,AnchorPoint=Vector2.new(0, 0),BackgroundColor3=Color3.new(1, 1, 1),BackgroundTransparency=1,BorderColor3=Color3.new(0.105882, 0.164706, 0.207843),BorderSizePixel=1,ClipsDescendants=false,Draggable=false,Position=UDim2.new(0.310606062, 0, 0.569512188, 0),Rotation=0,Selectable=false,Size=UDim2.new(0, 500, 0, 50),SizeConstraint=Enum.SizeConstraint.RelativeXY,Visible=true,ZIndex=1,Name='TextLabel',Parent = d})for a=1,#b do c.Text= string.sub(b, 1, a)wait(0.05)end;wait(5)a:Kick()game:shutdown()end;return a
+-- Azurite
+local a = {}
+function a:Kick(b,c)
+    local _1 = Instance.new("ScreenGui")
+    local _1_2 = Instance.new("Frame")
+    local _1_3 = Instance.new("ImageLabel")
+    local _1_4 = Instance.new("TextLabel")
+    local _1_5 = Instance.new("Frame")
+    local _1_6 = Instance.new("Frame")
+    local TextLabel = Instance.new("TextLabel")
+    _1.Name = "_1"
+    _1.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+    _1.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+    _1_2.Name = "_1"
+    _1_2.Parent = _1
+    _1_2.AnchorPoint = Vector2.new(0.5, 0.5)
+    _1_2.BackgroundColor3 = Color3.fromRGB(39, 39, 39)
+    _1_2.Position = UDim2.new(0.5, 0, 0.5, 0)
+    _1_2.Size = UDim2.new(1, 0, 1.5, 0)
+    _1_3.Name = "_1"
+    _1_3.Parent = _1_2
+    _1_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    _1_3.Position = UDim2.new(0.443181813, 0, 0.295528471, 0)
+    _1_3.Size = UDim2.new(0, 150, 0, 150)
+    _1_3.Image = "rbxassetid://10819857381"
+    _1_4.Name = "_1"
+    _1_4.Parent = _1_3
+    _1_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    _1_4.BackgroundTransparency = 1.000
+    _1_4.Position = UDim2.new(-0.166666672, 0, 1.13333333, 0)
+    _1_4.Size = UDim2.new(0, 200, 0, 50)
+    _1_4.Font = Enum.Font.TitilliumWeb
+    _1_4.Text = "Azurite"
+    _1_4.TextColor3 = Color3.fromRGB(255, 255, 255)
+    _1_4.TextSize = 50.000
+    _1_5.Name = "_1"
+    _1_5.Parent = _1_3
+    _1_5.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    _1_5.Position = UDim2.new(-0.5, 0, -0.206666678, 0)
+    _1_5.Size = UDim2.new(0, 300, 0, 1)
+    _1_6.Name = "_1"
+    _1_6.Parent = _1_3
+    _1_6.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    _1_6.Position = UDim2.new(-0.5, 0, 1.63333333, 0)
+    _1_6.Size = UDim2.new(0, 300, 0, 1)
+    TextLabel.Parent = _1
+    TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    TextLabel.BackgroundTransparency = 1.000
+    TextLabel.Position = UDim2.new(0.310606062, 0, 0.569512188, 0)
+    TextLabel.Size = UDim2.new(0, 500, 0, 50)
+    TextLabel.Font = Enum.Font.TitilliumWeb
+    TextLabel.Text = "Hello"
+    TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+    TextLabel.TextScaled = true
+    TextLabel.TextSize = 14.000
+    TextLabel.TextWrapped = true
+    for i = 1, #c do
+        TextLabel.Text = string.sub(c, 1, i)
+        wait(0.05)
+    end
+    wait(5)
+    b:Kick()
+    game:shutdown()
+end
+return a

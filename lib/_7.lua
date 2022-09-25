fa466c9a217beeef70c24a04f183d0ed070f4f3d
DaFrenchTokio <91798175+DaFrenchTokio@users.noreply.github.com>
2022-09-25 20:34:47 +0200

    Update _7.lua

--- lib/_7.lua
+++ lib/_7.lua
@@ -2,8 +2,12 @@ local a = {
     icon = "rbxthumb://type=Asset&id=10912483183&w=150&h=150"
 }
 function a.n()
-    pcall(function() 
-        game.StarterGui:SetCore("SendNotification", {Title = "Azurite",Text = msg,Icon = a.icon,Duration = 5})
-    end)
+	if shared.settings_azurite then
+		if shared.settings_azurite.notifications == true then
+			pcall(function() 
+				game.StarterGui:SetCore("SendNotification", {Title = "Azurite",Text = msg,Icon = a.icon,Duration = 5})
+			end)
+		end
+	end
 end
 return a

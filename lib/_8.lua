71a17e88e31351cd590e72f3e6a917e2f1f14b0f
DaFrenchTokio <91798175+DaFrenchTokio@users.noreply.github.com>
2022-10-26 02:56:05 +0200

    Update _8.lua

--- lib/_8.lua
+++ lib/_8.lua
@@ -1,5 +1,6 @@
 local a = {}
 function a:GetExploit()
 	local api = is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or secure_load and "Sentinel" or is_oxygen_function() and "Oxygen U" or KRNL_LOADED and "Krnl" or fluxus and "Fluxus" or getexecutorname() and "ScriptWare" or identifyexecutor() and "Arceus X" or WRD and "WeAreDevs Api" or IsElectron and "Electron" or "None" 
+	return api
 end
 return a

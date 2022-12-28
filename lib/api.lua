7685ece7e4aed8e42adf0cc4f3d19b18b96125e4
DaFrenchTokio <91798175+DaFrenchTokio@users.noreply.github.com>
2022-12-28 22:22:51 +0100

    Add files via upload

---
+++ lib/api.lua
@@ -0,0 +1,5 @@
+getgenv().Azurite = {}
+function Azurite.print(msg) azuriteconsole.print(msg) end
+function Azurite.say(msg) azu:Say(msg) end
+function Azurite:Kick(reason) azu:Kick(LP,reason) end
+function Azurite:Chat(msg) azu:Chat(msg) end
